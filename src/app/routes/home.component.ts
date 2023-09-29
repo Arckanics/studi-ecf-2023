f061937ec709138f31df471495e1d33eb37f9669
Arckanics <arckanics.reiko@gmail.com>
2023-09-29 14:07:37 +0200

    desktop layout for home component

--- src/app/routes/home.component.ts
+++ src/app/routes/home.component.ts
@@ -6,18 +6,59 @@ import { HomeFeatureService } from "../services/home-feature.service";
 @Component({
   selector: 'app-home',
   template: `
-    <div class="container-lg m-auto row row-cols-1 row-cols-lg-2">
-      <div>
-        <h3 class="h4 f-rajdhani">Services</h3>
-        <div id="services" class="row row-cols-1 g-4">
+    <div id="home" class="container-lg m-auto h-100 d-flex pt-2">
+      <div id="services" class="flex-grow-1 flex-shrink-1 home-component">
+        <h3 class="h4 f-rajdhani component-title">Services</h3>
+        <hr class="m-1">
+        <div class="list">
           <app-car-service *ngFor="let feat of features$" [feat]="feat"></app-car-service>
         </div>
       </div>
-      <div id="comment" class="row row-cols-1 g-4">
+      <div id="comment" class="flex-grow-1 flex-shrink-1 home-component">
+        <h3 class="h4 f-rajdhani component-title">Commentaires</h3>
+        <hr class="m-1">
+        <div class="list">
+          <app-car-service *ngFor="let feat of features$" [feat]="feat"></app-car-service>
+        </div>
       </div>
     </div>
   `,
-  styles: []
+  styles: [
+    `
+      :host {
+        height: 100%;
+        display: block;
+        overflow: hidden;
+        & > * {
+          display: block;
+        }
+      }
+      .home-component {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        .component-title {
+          padding: 0 .9rem;
+          margin: 0;
+        }
+        .list {
+          display: flex;
+          flex-direction: column;
+          gap: .8rem;
+          padding: 0 .9rem;
+          padding-bottom: 1rem;
+          overflow-y: auto;
+        }
+      }
+
+      #services {
+        position: relative;
+        height: 100%;
+      }
+    `
+  ]
 })
 export class HomeComponent implements OnInit {
 
