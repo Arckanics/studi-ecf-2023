c32ce49f7c105e9b834d90319088543c6fc29b0d
Arckanics <arckanics.reiko@gmail.com>
2023-10-10 19:30:43 +0200

    fix bad routing children module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FrontComponent } from "./front/front.component";
-import { HomeComponent } from "./routes/home.component";
-import { VehiclesComponent } from "./routes/vehicles.component";
 
 const routes: Routes = [
   {
-    path: "",
-    component: FrontComponent,
-    children: [
-      { path: "", component: HomeComponent },
-      { path: "vehicles", component: VehiclesComponent }
-    ]
+    path: "front-modules",
+    loadChildren: () => import('./front/front.module').then(m => m.FrontModule)
   }
 ];
 
--- src/app/front/front-routing.module.ts
+++ src/app/front/front-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "../routes/home.component";
 import { VehiclesComponent } from "../routes/vehicles.component";
+import { FrontComponent } from "./front.component";
 
 const routes: Routes = [
-  {path:"vehicles",component:VehiclesComponent},
-  {path:"",component:HomeComponent},
+  {
+    path:"",component:FrontComponent,
+    children: [
+      {path:"vehicles",component:VehiclesComponent},
+      {path:"",component:HomeComponent},
+    ]
+  }
 ];
 
 @NgModule({
