c2eb76ac3ade77a8e3363dcecc3c9f758f723f7b
Arckanics <arckanics.reiko@gmail.com>
2023-10-27 14:30:34 +0200

    update crud

--- front/src/app/admin/component/hours/hours.component.ts
+++ front/src/app/admin/component/hours/hours.component.ts
@@ -12,15 +12,18 @@ export type Hour = {
   selector: 'admin-hours',
   template: `
     <div class="row align-items-center w-100">
-      <div class="col-6 col-md-3"><span class="fw-bold">Jour: </span> <span class="text-capitalize">{{strDay}}</span>
+      <div class="col-6 col-md-3"><span class="fw-bold">Jour: </span> <span
+        class="text-capitalize">{{getStrDay()}}</span>
       </div>
       <div class="col-6 col-md-3"><span class="fw-bold">Début: </span> <span>{{hour.begin}}</span></div>
       <div class="col-6 col-md-3"><span class="fw-bold">Fin: </span> <span>{{hour.end}}</span></div>
     </div>
     <div class="flex-shrink-0">
       <div class="d-flex align-items-end justify-content-end flex-md-row flex-column gap-2 actions">
-        <div role="button" class="btn btn-warning d-block" (click)="putAction(['edit','hours',hour.id])"><i class="bi bi-pencil-square"></i></div>
-        <div role="button" class="btn btn-dark d-block" (click)="putAction(['delete','hours',hour.id])"><i class="bi bi-trash3"></i></div>
+        <div role="button" class="btn btn-warning d-block" (click)="putAction(['edit','hours',hour.id])"><i
+          class="bi bi-pencil-square"></i></div>
+        <div role="button" class="btn btn-dark d-block" (click)="putAction(['delete','hours',hour.id])"><i
+          class="bi bi-trash3"></i></div>
       </div>
     </div>
   `,
@@ -41,17 +44,20 @@ export type Hour = {
 })
 export class HoursComponent extends AbstractComponent {
   @Input() hour!: Hour
-  strDay!: string
 
   constructor() {
     super()
   }
 
   ngOnInit() {
+    this.getStrDay()
+  }
+
+  getStrDay() {
     const { day } = this.hour
     let date = new Date()
     let reset = date.getDay() - 1
     date.setDate(date.getDate() - reset + day)
-    this.strDay = date.toLocaleDateString('fr-FR', { weekday: "long" })
+    return date.toLocaleDateString('fr-FR', { weekday: "long" })
   }
 }
